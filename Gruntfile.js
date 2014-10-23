13b06c7490b832d27f528983998311b3d1114430
bondydaa <bondydaa@gmail.com>
2014-10-22 20:12:07 -0500

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      compass: {
+        files: ['src/styles/*.scss'],
+        tasks: ['compass:dist']
+      },
+      concat: {
+        files: ['src/js/{,*/}*.js'],
+        tasks: ['concat:dist']
+      }
+    },
+
+    // Compiles Sass to CSS and generates necessary files if requested
+    compass: {
+      options: {
+        sassDir: 'src/styles/',
+        cssDir: 'www/css/',
+        relativeAssets: false,
+        assetCacheBuster: false,
+        outputStyle: 'expanded',
+        noLineComments: false
+      },
+      dist: {
+        options: {
+          noLineComments: true
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: grunt.util.linefeed + grunt.util.linefeed,
+        banner: '(function() {' + grunt.util.linefeed + grunt.util.linefeed,
+        footer: grunt.util.linefeed + grunt.util.linefeed + '})();'
+      },
+      dist: {
+        src: ['src/js/*.js'],
+        dest: 'www/js/crispin-mulberry.js',
+      },
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', [
+    'concat:dist',
+    'compass:dist'
+  ]);
+
+};
