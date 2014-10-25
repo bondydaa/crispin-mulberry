06765f940d25e1c205de5dd0be573af83c7354fd
bondydaa <bondydaa@gmail.com>
2014-10-25 13:20:40 -0500

    setting up page skeleton and css

---
+++ src/js/test.js
@@ -0,0 +1,12 @@
+  var test = function(){
+    console.log('test');
+  }
+
+
+  var moreTesting = function(e,z) {
+    var testedee = e;
+    var pleaseWork = z;
+    var newVar = function(){
+      alert();
+    };
+  }
